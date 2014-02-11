9742417ad7caca32586fc89ff28f9a51c81b2060
saml <skynare@gmail.com>
2014-02-10 21:42:06 -0500

    asdf

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
+var ENDSWITH_WHITESPACE = /\s+$/;
+var STARTSWITH_WHITESPACE = /^\s+/;
+var INLINE_ELEMENTS = {
+    b:true, big:true, i:true, small:true, tt:true,
+    abbr:true, acronym:true, cite:true, code:true, dfn:true, em:true, kbd:true, strong:true, samp:true, 'var':true,
+    a:true, bdo:true, br:true, img:true, map:true, object:true, q:true, script:true, span:true, sub:true, sup:true,
+    button:true, input:true, label:true, select:true, textarea:true
+};
+
 function trim(input) {
     return input.replace(/^\s+/, '').replace(/\s+$/, '');
 }
 
-function writeln(output, line, level) {
-    //var indentation = level*4;
-    //output.innerHTML += (Array(indentation + 1).join(' ') + line +'\n');
-    output.innerHTML += line;
+function writeln(output, line, level, insertWhitespace) {
+    var indentation = level*4;
+    if (insertWhitespace) {
+        output.innerHTML += ('\n'+Array(indentation + 1).join(' ') + line);
+    } else {
+        output.innerHTML += line;
+    }
 }
 
 function attributesToString(node) {
@@ -14,18 +26,27 @@ function attributesToString(node) {
     var buffer = [''];
     for (var i = 0; i < n; i++) {
         var attrib = attributes.item(i);
-        buffer.push(attrib.name + '="' + attrib.value + '"');
+        if (attrib.value) {//for attribute="false" might fail.
+            buffer.push(attrib.name + '="' + attrib.value + '"');
+        } else {
+            buffer.push(attrib.name);
+        }
     }
     return buffer.join(' ');
 }
 
+function isInlineElement(node) {
+    return node.nodeName.toLowerCase() in INLINE_ELEMENTS;
+}
+
+
 function printNode(node, output, prevLevel, level) {
     switch (node.nodeType) {
     case 1: //element
-        writeln(output, '<' + node.nodeName.toLowerCase() + attributesToString(node) + '>', level);
+        writeln(output, '<' + node.nodeName.toLowerCase() + attributesToString(node) + '>', level, !isInlineElement(node));
         break;
     case 3: //text
-        writeln(output, node.nodeValue, prevLevel);
+        writeln(output, trim(node.nodeValue), prevLevel);
         break;
     case 2:
         alert(node);
@@ -34,36 +55,33 @@ function printNode(node, output, prevLevel, level) {
 }
 
 function closeNode(node, output, level) {
-    if (node.nodeType === 1) {
+    if (node.nodeType === 1 && node.childNodes.length > 0) {
         writeln(output, '</' + node.nodeName.toLowerCase() + '>', level);
     }
 }
 
 function prettyPrintRecurr(node, output, level) {
-    printNode(node, output, level, level);
-    node = node.firstChild;
     while (node) {
-        var nextLevel = level + 1;
-        prettyPrintRecurr(node, output, nextLevel);
-        closeNode(node, output, nextLevel);
+        printNode(node, output, level, level);
+        prettyPrintRecurr(node.firstChild, output, level+1);
+        closeNode(node, output, level);
         node = node.nextSibling;
     }
 }
 
 function prettyPrint(node, output) {
     output.innerHTML = '';
-    prettyPrintRecurr(node, output, 0);
-    closeNode(node, output, 0);
+    prettyPrintRecurr(node.firstChild, output, 0);
+    //closeNode(node, output, 0);
 }
 
 function main() {
-    var wrap = document.getElementById('wrap');
     var reinput = document.getElementById('reinput');
     var tohtml = document.getElementById('tohtml');
     reinput.onclick = function () {
         var output = document.getElementById('output');
         var input = document.getElementById('input');
-        wrap.innerHTML = output.textContent;
+        input.innerHTML = output.textContent;
         prettyPrint(input, output);
     };
     tohtml.onclick = function () {
